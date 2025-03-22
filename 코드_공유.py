1f031e24aa82536c28f45cd6ef9c7f9bfc262875
userA <userA@userA.com>
2025-03-22 15:47:52 +0900

    userA 작업 완료

--- 코드_공유.py
+++ 코드_공유.py
@@ -6,7 +6,11 @@ warnings.filterwarnings('ignore')
 import pandas as pd
 from sklearn.datasets import load_wine
 
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
+
+from xgboost import XGBClassifier
 
 import matplotlib.pyplot as plt
 
@@ -22,3 +26,38 @@ X = df.drop('target',axis=1)
 y = df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+param_grid = {
+    "criterion" : ['gini', 'entropy'],
+    "max_depth" : [2,5],
+    "min_samples_split": [2, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
+
+clf_grid = DecisionTreeClassifier( random_state= 42 )
+grid_search = GridSearchCV(clf_grid, param_grid, cv = 5)
+grid_search.fit(X_train, y_train)
+
+print("Best Hyper-parameter", grid_search.best_params_)
+print("Best Score", grid_search.best_score_)
+
+best_model = grid_search.best_estimator_
+
+y_pred_grid = best_model.predict(X_test)
+accuracy_grid_DT = accuracy_score(y_test, y_pred_grid)
+print('Accuracy Grid :', accuracy_grid_DT)
+print("\n Classification Report", classification_report(y_test, y_pred_grid))
+report_DT = classification_report(y_test, y_pred_grid)
+
+# Feature Importance를 계산
+importances = best_model.feature_importances_
+
+# Best model의 Feature Importance를  시각화
+plt.figure(figsize = (20,6))
+# 막대 그래프 생성
+plt.bar(range(len(importances)), importances, width=0.3)
+plt.xlabel('Feature')
+plt.ylabel('importances')
+plt.title('Feature Importance')
+plt.xticks(range(len(importances)), X.columns, rotation = 45)
+plt.show()
